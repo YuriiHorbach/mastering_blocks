f0bc82bca974a84b794a37b632df8ee8355978f4
user <gyrii84@gmail.com>
2021-02-02 21:50:40 +0200

    add application app toggle around yield

---
+++ aplication.rb
@@ -0,0 +1,39 @@
+class Application
+  attr_accessor :environment
+
+  def initialize
+    @environment = :development
+  end
+
+  def connect_to_database
+    puts "Connecting to #{@environment} database..."
+  end
+
+  def handle_request
+    puts "Handling #{@environment} request..."
+  end
+
+  def write_to_log
+    puts "Writing to #{@environment} log file..."
+  end
+
+  def in_production
+    default_environment = :@environment
+    yield
+    @environment = :development
+    puts "Reset environment to #{self.environment}"
+  end
+end
+
+app = Application.new
+# app.connect_to_database
+# app.handle_request
+# app.write_to_log
+
+
+app.in_production do
+  app.connect_to_database
+  app.handle_request
+  raise "Boom!"
+  app.write_to_log
+end
